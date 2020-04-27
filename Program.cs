870f9f9e4d71101234ca1f6216e400e3cb48c825
Lluminatarion-8431 <captain.a.8431@gmail.com>
2020-04-26 22:17:26 -0700

    I created a for loop that will loop 5 times, I also created a for loop that will loop a users input 9 times, and I created another for loop that will loop user input as many times as the user wants. Also, i created a for loop that will output the index of each characters in a string.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace ForLoopCriticalThinking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+
+            //};
+            //for (int i = 9; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.Write("Enter the word you would like to print out: ");
+            //string word = Console.ReadLine();
+
+            //Console.Write("Enter the amount of times you would like to print " + word + ": ");
+            //int number = Int32.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < number; i++)
+            //    Console.WriteLine(word);
+
+            //int numberOfLetters = 0;
+            //foreach (char letter in "Hello World")
+            //{
+            //    Console.WriteLine(numberOfLetters++);
+            //}
+
+            //string text = "Hello World";
+            //foreach (char c in text)
+            //{
+            //    Console.Write(c);
+            //};
+            //(i+2)
+
+
+            //int i;
+            //string str = "Hello World";
+            //for (i = 0; i < str.Length; i++)
+            //{
+            //    Console.Write(str[i]);
+
+            //};
+            foreach (char c in "this is a test")
+                Console.WriteLine(c);
+
+
+        }
+
+    }
+}
